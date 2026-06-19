be0dbf5097a4bc3dc5071c5812217485808dcb3e
Cecilia Grace <cecigrace2003@gmail.com>
2026-06-19 14:32:19 +0300

    completed the song.py file

--- lib/song.py
+++ lib/song.py
@@ -1,63 +1,28 @@
 class Song:
-    _count = 0  
+    count = 0
     genre_count = {}
     artist_count = {}
-    
-    _genres = set()
-    _artists = set()
-    
+    genres = []
+    artists = []
+
     def __init__(self, name, artist, genre):
-        if Song._count == 0:
+        if Song.count == 0:
             Song.genre_count.clear()
             Song.artist_count.clear()
-            Song._genres.clear()
-            Song._artists.clear()
-        
+            Song.genres.clear()
+            Song.artists.clear()
+
         self.name = name
         self.artist = artist
         self.genre = genre
+
+        Song.count += 1
         
-        Song._count += 1
-        
-        Song._genres.add(genre)
-        Song._artists.add(artist)
-        
-        if genre in Song.genre_count:
-            Song.genre_count[genre] += 1
-        else:
-            Song.genre_count[genre] = 1
+        if genre not in Song.genres:
+            Song.genres.append(genre)
             
-        if artist in Song.artist_count:
-            Song.artist_count[artist] += 1
-        else:
-            Song.artist_count[artist] = 1
-        
-        
-    @classmethod
-    def count(cls):
-        return cls._count
-    
-    @classmethod
-    def genres(cls):
-        return list(cls._genres)
-    
-    @classmethod
-    def artists(cls):
-        return list(cls._artists)
-        
-                
+        if artist not in Song.artists:
+            Song.artists.append(artist)
 
-song1 = Song("99 Problems", "Jay Z", "Rap")
-song2 = Song("Halo", "Beyonce", "Pop")
-song3 = Song("Smells Like Teen Spirit", "Nirvana", "Rock")
-song4 = Song("Out of Touch", "Hall and Oates", "Pop")
-song5 = Song("Drunk In Love", "Beyonce", "Pop")
-song6 = Song("Show You Off", "Justin Bieber", "RnB")
-
-
-    
-print(Song.count())
-print(Song.genres())
-print(Song.artists())
-print(Song.genre_count)
-print(Song.artist_count)
+        Song.genre_count[genre] = Song.genre_count.get(genre, 0) + 1
+        Song.artist_count[artist] = Song.artist_count.get(artist, 0) + 1
